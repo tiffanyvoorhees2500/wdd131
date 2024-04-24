c1106dcb2b8500a4c11698fff10b1979c52dcd1e
tiffster2500 <tiff6377@hotmail.com>
2024-04-24 10:32:02 -0600

    basic-layout updates and finished index

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+const currentyear_element = document.querySelector("#currentyear");
+currentyear_element.innerHTML = ` ${today.getFullYear()}`;
+
+const lastModified_element = document.querySelector("#lastModified");
+lastModified_element.innerHTML = "Last Modification: " + document.lastModified
